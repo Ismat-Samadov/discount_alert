1aa2884be5d3a66622c802eef51cf20549d6ae27
Ismat-Samadov <ismetsemedov@gmail.com>
2023-10-15 22:10:43 +0400

    check

--- main.py
+++ main.py
@@ -10,14 +10,6 @@ from datetime import datetime
 from email.mime.text import MIMEText
 import io
 
-# async def fetch_page(session, page_number):
-#     url = f"https://umico.az/categories/16-noutbuklar?page={page_number}&q%5Bs%5D=discount%20desc"
-#     async with session.get(url) as response:
-#         if response.status == 200:
-#             return await response.text(encoding='utf-8')
-#         else:
-#             print(f"Failed to retrieve the webpage. Status code:", response.status)
-#             return None
 async def fetch_page(session, page_number):
     max_retries = 3  # Define the maximum number of retries
     retries = 0
@@ -53,7 +45,7 @@ async def scrape_page(page_content, discount_percentages, discounted_prices, rea
         discounted_price = item.find('span', class_='MPPrice-RetailPrice')
         if discounted_price:
             discounted_price = discounted_price.text.strip().replace('₼', '').replace(' ', '')
-        else:
+        else:   
             discounted_price = '0'
         
         real_price = item.find('span', class_='MPPrice-OldPrice')
@@ -97,8 +89,23 @@ async def main():
     product_hrefs = []
 
     tasks = []
-
-    async with aiohttp.ClientSession() as session:
+   
+    headers = {
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8,ru;q=0.7,az;q=0.6',
+            'Dnt': '1',
+            'Origin': 'https://umico.az',
+            'Referer': 'https://umico.az/',
+            'Sec-Ch-Ua': '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
+            'Sec-Ch-Ua-Mobile': '?0',
+            'Sec-Ch-Ua-Platform': '"Windows"',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'cross-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'
+        }
+    async with aiohttp.ClientSession(trust_env=True,headers=headers) as session:
         for page_number in range(1, 110):
             task = asyncio.create_task(fetch_page(session, page_number))
             tasks.append(task)
@@ -126,26 +133,28 @@ async def main():
     # Create an email message
     MY_EMAIL = "ismetsemedov@gmail.com"
     MY_PASSWORD = "lmjareknmmweotsp"
-    TO_EMAIL = "ismetsemedli@mail.ru"
-    subject = "Discounts"
+    current_date = datetime.now().strftime("%d.%m.%Y")
+    subject = "🔥🔥🔥Discounts "  + current_date + "🔥🔥🔥"
 
-    msg = MIMEText(f'<html><body>{table_html}</body></html>', 'html', 'utf-8')
-    msg['Subject'] = subject
-    msg['From'] = MY_EMAIL
-    msg['To'] = TO_EMAIL
+    TO_EMAIL = "ismetsemedli@mail.ru,Ismat.Samadov@kapitalbank.az,ismetsemedov@gmail.com,allahverdiyev.tural@hotmail.com,kamalkhalilov7@gmail.com,shukurovvusal@gmail.com,abdiyev-mubariz@mail.ru"
+    recipient_emails = TO_EMAIL.split(',')
 
-    # Send the email
-    try:
-        connection = smtplib.SMTP("smtp.gmail.com", 587)
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        current_date = datetime.now().strftime("%d.%m.%Y")
-        msg_str = msg.as_string()
-        connection.sendmail(from_addr=MY_EMAIL, to_addrs=TO_EMAIL, msg=msg_str)
-        connection.quit()
-        print("Email sent successfully.")
-    except Exception as e:
-        print("An error occurred:", str(e))
+    for recipient_email in recipient_emails:
+        try:
+            connection = smtplib.SMTP("smtp.gmail.com", 587)
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            msg = MIMEText(f'<html><body>{table_html}</body></html>', 'html', 'utf-8')
+            msg['Subject'] = subject
+            msg['From'] = MY_EMAIL
+            msg['To'] = recipient_email
+            msg_str = msg.as_string()
+            connection.sendmail(from_addr=MY_EMAIL, to_addrs=recipient_email, msg=msg_str)
+            connection.quit()
+            print(f"Email sent successfully to {recipient_email}.")
+        except Exception as e:
+            print(f"An error occurred while sending the email to {recipient_email}: {str(e)}")
+
 
 if __name__ == '__main__':
     asyncio.run(main())
